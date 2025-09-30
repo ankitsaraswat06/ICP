06fe54de8492a7fde99e6ff06b9f049884867cfd
Ankit Saraswat <ankitsaraswat@Ankits-MacBook-Air.local>
2025-09-30 21:11:18 +0530

    added 1st question

---
+++ Assignment4/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,25 @@
+package Assingnment4;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int longest(String s) {
+        int[] freq = new int[255];
+        int si = 0;
+        int ei = 0;
+        int ans = 0;
+        while(ei<s.length()) {
+            while(freq[s.charAt(ei)] > 0) {
+                freq[s.charAt(si)]--;
+                si++;
+            }
+            freq[s.charAt(ei)]++;
+            
+            ans = Math.max(ans, ei-si+1);
+            ei++;
+        }
+        return ans;
+    }
+    public static void main(String args[]) {
+        String s = "abcabcbb";
+        System.out.println(longest(s));
+    }
+}
